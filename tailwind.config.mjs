464557ccdd1b07032b24f0265662ddadfc2c94c1
A1igator <aliabdoli1970@gmail.com>
2024-05-05 19:30:46 -0400

    fix drak theme and navigation

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	darkMode: "class", // or 'media' based on preference
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+	// ...rest of the config
+  };
+  
