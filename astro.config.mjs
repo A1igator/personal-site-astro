e2eb2ad8516c7719681ee1f7c2c79f840996a20e
A1igator <aliabdoli1970@gmail.com>
2024-05-14 10:09:12 -0400

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), tailwind(), mdx()],
   site: "https://www.aliabdoli.com",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
